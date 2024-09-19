befd4dc6e9c84a00c8c29ed151d8f7561c77c3fc
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2024-09-19 20:31:31 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8000/api/v1/user/patient/me",
+          "https://mern-stack-hospital-management.vercel.app/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
