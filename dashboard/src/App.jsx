eceb2ecc5f95e636984c44bb5d97c5200c6c4c50
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2024-09-19 20:33:52 +0700

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8000/api/v1/user/admin/me",
+          "https://mern-stack-hospital-management-sevk.vercel.app/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
